b6b07e1f03a8c8663a941408ab4e2aa6d0bd99af
jom1games <jonas@saslona.com>
2024-12-08 11:36:50 +0100

    config for remove command

--- src/main/java/de/jonas/customentities/commands/Remove.java
+++ src/main/java/de/jonas/customentities/commands/Remove.java
@@ -1,18 +1,22 @@
 package de.jonas.customentities.commands;
 
+import org.bukkit.configuration.file.FileConfiguration;
+
 import de.jonas.customentities.CustomEntitiesList;
+import de.jonas.customentities.Custom_entities;
 import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.arguments.StringArgument;
 
 public class Remove {
 
     CustomEntitiesList cel = new CustomEntitiesList();
+    FileConfiguration config = Custom_entities.CONFIG;
 
     public Remove() {
 
         new CommandAPICommand("custom-entities:remove")
-                .withAliases("ce:remove")
-                .withPermission("CustomEntities.remove")
+            .withAliases(config.getStringList("Commands.Remove.aliases").toArray(num -> new String[num]))
+            .withPermission(config.getString("Commands.Remove.permission"))
                 .withArguments(new StringArgument("name"))
                 .executesPlayer(((player, commandArguments) -> {
                     String name = (String) commandArguments.get("name");
