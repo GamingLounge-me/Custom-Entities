f20f73aafc45997a1f101d5b58609a98815a14e1
jom1games <jonas@saslona.com>
2024-12-08 11:33:42 +0100

    use config in admin command

--- src/main/java/de/jonas/customentities/commands/Admin.java
+++ src/main/java/de/jonas/customentities/commands/Admin.java
@@ -1,19 +1,23 @@
 package de.jonas.customentities.commands;
 
-import de.jonas.customentities.CustomEntitiesList;
-import dev.jorel.commandapi.CommandAPICommand;
-
 import java.util.ArrayList;
+
+import org.bukkit.configuration.file.FileConfiguration;
+
+import de.jonas.customentities.CustomEntitiesList;
+import de.jonas.customentities.Custom_entities;
+import dev.jorel.commandapi.CommandAPICommand;
 
 public class Admin {
 
     CustomEntitiesList cel = new CustomEntitiesList();
+    FileConfiguration config = Custom_entities.CONFIG;
 
     public Admin() {
 
-        new CommandAPICommand("ce:admin")
-                .withAliases("custom-entities:admin")
-                .withPermission("CustomEntities.admin")
+        new CommandAPICommand("custom-entities:admin")
+            .withAliases(config.getStringList("Commands.Admin.aliases").toArray(num -> new String[num]))
+                .withPermission(config.getString("Commands.Admin.permission"))
                 .withSubcommand(new CommandAPICommand("list")
                         .executesPlayer(((player, commandArguments) -> {
                             player.sendMessage(String.valueOf(cel.getList(player.getWorld())));
